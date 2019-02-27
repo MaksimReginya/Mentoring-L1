7ac55efbf77467ec843f8965582e02f5cd366338
MaksimReginya <maksim.reginya@gmail.com>
2019-02-27 19:53:23 +0300

    Add .Net Framework and .Net Core tasks

---
+++ 1st module - Introduction to .Net/WindowsFormsApp/WindowsFormsApp/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnHello_Click(object sender, EventArgs e)
+		{
+			string name = this.tbInputName.Text;
+
+			MessageBox.Show($"Hello, {name}!");
+		}
+	}
+}
