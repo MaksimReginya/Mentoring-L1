c5adafa19f11c7b8142b822519d4d6a882962d91
MaksimReginya <maksim.reginya@gmail.com>
2019-02-27 23:01:20 +0300

    Add Mono Gtk App

---
+++ 1st module - Introduction to .Net/MonoGtkApp/MonoGtkApp/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBtnHelloClicked(object sender, EventArgs e)
+    {
+        MessageDialog messageDialog = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Hello, {0}!", this.entryName.Text);
+
+        messageDialog.Run();
+        messageDialog.Destroy();
+    }
+}
