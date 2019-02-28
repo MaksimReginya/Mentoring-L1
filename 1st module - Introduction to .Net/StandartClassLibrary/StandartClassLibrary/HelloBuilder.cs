14177a58fd15f16618665bbf31fb959a59511211
MaksimReginya <maksim.reginya@gmail.com>
2019-02-28 22:34:45 +0300

    Add .Net Standart class library

---
+++ 1st module - Introduction to .Net/StandartClassLibrary/StandartClassLibrary/HelloBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StandartClassLibrary
+{
+	public static class HelloBuilder
+	{
+		public static string BuildHelloMessage(string name)
+		{
+			string currentTime = DateTime.Now.ToShortTimeString();
+
+			return $"{currentTime} Hello, {name}!";
+		}
+	}
+}
