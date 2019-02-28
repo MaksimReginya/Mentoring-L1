5b54bbe140587a65dd4a235036763f07768b5fcd
Maksim Rehinia <Maksim_Rehinia@epam.com>
2019-02-28 14:15:34 +0300

    Add Xamarin solutions

---
+++ 1st module - Introduction to .Net/XamarinNative/XamarinNative/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace XamarinNative
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+			Button btn = FindViewById<Button>(Resource.Id.btnHello);
+			btn.Click += this.Btn_Click;
+        }
+
+		private void Btn_Click(object sender, System.EventArgs e)
+		{
+			TextView tvName = FindViewById<TextView>(Resource.Id.tvName);
+
+			tvName.Text = "Hello, world!";
+		}
+	}
+}
