0cef28d8d1193b2340df076cbba745bf62dd6e1c
MaksimReginya <maksim.reginya@gmail.com>
2019-03-17 21:15:17 +0300

    Create NumberParser solution

---
+++ 3d module - Exception handling/NumberParser/NumberParser/NumberParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumberParser
+{
+    public static class NumberParser
+	{
+		public static int ConvertToNumber(this string number)
+		{
+			if (string.IsNullOrEmpty(number))
+			{
+				throw new ArgumentNullException(nameof(number), $"{nameof(number)} cannot be null or empty");
+			}
+
+			return 0;
+		}
+    }
+}
