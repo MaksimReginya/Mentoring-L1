e6fa6bddbedc55ab6e2f0a4c7a83d063136d0fd2
Maksim Rehinia <Maksim_Rehinia@epam.com>
2019-03-18 14:24:01 +0300

    Update NumberParser logic

--- 3d module - Exception handling/NumberParser/NumberParser/NumberParser.cs
+++ 3d module - Exception handling/NumberParser/NumberParser/NumberParser.cs
@@ -11,7 +11,45 @@ namespace NumberParser
 				throw new ArgumentNullException(nameof(number), $"{nameof(number)} cannot be null or empty");
 			}
 
-			return 0;
+			char[] digits = number.ToCharArray();
+			int result = 0, i = 0;
+			if (digits.Length == 1)
+			{
+				ValidateDigit(digits[0]);
+				result = digits[0] - '0';
+			}
+
+			bool isNegative = IsNegative(digits[0]);
+			if (isNegative)
+			{
+				i = 1;
+			}
+
+			for (; i < digits.Length; i++)
+			{
+				ValidateDigit(digits[i]);
+				int temp = digits[i] - '0';
+				if (temp != 0)
+				{
+					result += temp * (int)Math.Pow(10, (digits.Length - (i + 1)));
+				}
+			}
+
+			return isNegative ? -result : result;
 		}
-    }
+
+		private static void ValidateDigit(char el)
+		{
+			if (!char.IsDigit(el))
+			{
+				throw new FormatException();
+			}
+		}
+
+		private static bool IsNegative(char el)
+		{
+			return '-'.Equals(el);
+		}
+
+	}
 }
