1de6773e6f67635aa030061dd7a629d2cfc6e82d
MaksimReginya <maksim.reginya@gmail.com>
2019-05-12 00:02:30 +0300

    Create Northwind HTTP handler

---
+++ 8th module - HTTP fundamentals/NorthwindHttpHandler/NorthwindHttpHandler/NorthwindHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Web;
+using NorthwindHttpHandler.ReportGenerator;
+
+namespace NorthwindHttpHandler
+{
+	public class NorthwindHandler : IHttpHandler
+	{
+		private readonly string[] _xmlAcceptTypes = { "text/xml", "application/xml" };
+
+		public bool IsReusable => true;
+
+		public void ProcessRequest(HttpContext context)
+		{
+			NameValueCollection queryString;
+			if (context.Request.QueryString != null)
+			{
+				queryString = context.Request.QueryString;
+			}
+			else
+			{
+				queryString = this.ParseRequestBody(context.Request);
+			}
+
+			ReportFormat format = this.ParseReportFormat(context.Request);
+			//StringBuilder s = new StringBuilder();
+			using (DataModel model = new DataModel())
+			{
+				Generator generator = new Generator(model.Orders.AsQueryable(), queryString, format);
+				/*foreach (var item in model.Orders)
+				{
+					s.Append($"{item.OrderDate}, {item.CustomerID}");
+				}*/
+			}
+
+			
+			//context.Response.Output.WriteLine(s.ToString());
+		}
+
+		private ReportFormat ParseReportFormat(HttpRequest request)
+		{
+			if (_xmlAcceptTypes.Intersect(request.AcceptTypes).Any())
+			{
+				return ReportFormat.Xml;
+			}
+
+			return ReportFormat.Xlsx;
+		}
+
+		private NameValueCollection ParseRequestBody(HttpRequest request)
+		{
+			string input;
+
+			using (StreamReader reader = new StreamReader(request.InputStream))
+			{
+				input = reader.ReadToEnd();
+			}
+
+			return HttpUtility.ParseQueryString(input);
+		}
+	}
+}
---
+++ 8th module - HTTP fundamentals/NorthwindHttpHandler/NorthwindHttpHandler/ReportGenerator/ReportGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using NorthwindHttpHandler.DataAccess.GeneratedEntities;
+using NorthwindHttpHandler.Exceptions;
+
+namespace NorthwindHttpHandler.ReportGenerator
+{
+	public class Generator
+	{
+		private IQueryable<Order> _orders;
+		private readonly NameValueCollection _queryString;
+		private readonly ReportFormat _format;
+
+		public Generator(IQueryable<Order> orders, NameValueCollection queryString, ReportFormat format)
+		{
+			_orders = orders;
+			_queryString = queryString;
+			_format = format;
+		}
+
+		public void CreateReport()
+		{
+			this.FilterByCustomerId();
+			this.FilterByDate();
+			this.FilterByTake();
+			this.FilterBySkip();
+
+		}
+
+		private void FilterOrders()
+		{
+			
+		}
+
+		private void FilterByCustomerId()
+		{
+			string customerId = _queryString["customer"];
+			if (!string.IsNullOrEmpty(customerId))
+			{
+				_orders = _orders.Where(order => order.CustomerID.Equals(customerId, StringComparison.OrdinalIgnoreCase));
+			}
+		}
+
+		private void FilterByDate()
+		{
+			string dateFrom = _queryString["dateFrom"];
+			string dateTo = _queryString["dateTo"];
+			if (!string.IsNullOrEmpty(dateFrom) && !string.IsNullOrEmpty(dateTo))
+			{
+				throw new InvalidRequestException("dateFrom and dateTo cannot be specified at the same time");
+			}
+			else if (!string.IsNullOrEmpty(dateFrom))
+			{
+				DateTime date = DateTime.Parse(dateFrom);
+				_orders = _orders.Where(order => order.OrderDate.Value >= date);
+			}
+			else if (!string.IsNullOrEmpty(dateTo))
+			{
+				DateTime date = DateTime.Parse(dateTo);
+				_orders = _orders.Where(order => order.OrderDate.Value <= date);
+			}
+		}
+
+		private void FilterByTake()
+		{
+			if (!int.TryParse(_queryString["take"], out int take))
+			{
+				return;
+			}
+
+			_orders = _orders.Take(take);
+		}
+
+		private void FilterBySkip()
+		{
+			if (!int.TryParse(_queryString["skip"], out int skip))
+			{
+				return;
+			}
+
+			_orders = _orders.Skip(skip);
+		}
+	}
+}
